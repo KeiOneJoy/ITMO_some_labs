f2ef50d6d215f6941b26a019dd0a271d9d1318e0
ilya <ilya.wens.art@gmail.com>
2024-01-09 16:35:52 +0300

    test
    test

---
+++ ITMO_m2_labs/ITMO_m2_labs/Custom_arr.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.CodeDom;
+
+namespace ITMO_m2_labs
+{
+    public class Custom_arr
+    {
+        private readonly double[] array;
+
+        public Custom_arr(int length)
+        {
+            if (length <= 0)
+                throw new ArgumentOutOfRangeException(nameof(length));
+
+            var random = new Random();
+
+            array = new double[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = random.Next(-10, 10);
+            }
+        }
+
+        public double GetMaxElement()
+        {
+            if (array.Length == 0)
+                throw new InvalidOperationException("Массив пуст");
+
+            double maxElement = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > maxElement)
+                {
+                    maxElement = array[i];
+                }
+            }
+
+            return maxElement;
+           
+        }
+      
+        public double GetSumUntilTheLastPositiveElement()
+        {
+            double sum = 0;
+            int lastIndex = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    lastIndex = i;
+                }
+
+                sum += array[i];
+            }
+
+            if (lastIndex != -1)
+            {
+                sum -= array[lastIndex];
+            }
+
+            return sum;
+        }
+
+
+
+
+
+        public void CompressArray(double a, double b)
+        {
+            int newIndex = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Math.Abs(array[i]) >= a && Math.Abs(array[i]) <= b)
+                {
+                    array[newIndex] = 0;
+                }
+                else
+                {
+                    array[newIndex] = array[i];
+                }
+            }
+
+            // Заполняем оставшиеся элементы нулями
+            for (int i = newIndex; i < array.Length; i++)
+            {
+                array[i] = 0;
+            }
+        }
+
+        public double[] GetArray()
+        {
+            return array;
+        }
+    }
+}
---
+++ ITMO_m2_labs/ITMO_m2_labs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ITMO_m2_labs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Часть 1:");
+            Console.Write("Введите размер массива: ");
+
+            int size = int.Parse(Console.ReadLine());
+
+            Custom_arr customArr = new Custom_arr(size);
+
+            double maxElement = customArr.GetMaxElement();
+            Console.WriteLine("Максимальный элемент в массиве: " + maxElement);
+
+            Console.WriteLine("Сгенерированный массив:");
+            foreach (double element in customArr.GetArray())
+            {
+                Console.Write(element + " ");
+            }
+
+            double sum = customArr.GetSumUntilTheLastPositiveElement();
+            Console.WriteLine("\nСумма элементов до последнего положительного элемента: " + sum);
+
+            Console.Write("\nВведите значение a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            customArr.CompressArray(a, b);
+
+            Console.WriteLine("\nМассив после сжатия:");
+            foreach (double element in customArr.GetArray())
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.WriteLine("\nНажмите Enter, чтобы завершить программу.");
+            Console.ReadLine(); // Ожидание пользовательского ввода
+        }
+    }
+}
