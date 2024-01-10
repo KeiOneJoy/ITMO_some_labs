02665c0bfdc0ca8be671a8b8ad8db958763474f4
ilya <ilya.wens.art@gmail.com>
2024-01-10 16:35:24 +0300

    bugs fix

---
+++ M1/Task01_02_/Labs01_02/Labs01_02/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO_labs_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Часть 1:");
+            Console.Write("Введите размер массива: ");
+
+            int size = int.Parse(Console.ReadLine());
+
+            Custom_arr customArr = new Custom_arr(size);
+
+            double maxElement = customArr.GetMaxElement();
+            Console.WriteLine("Максимальный элемент в массиве: " + maxElement);
+
+            Console.WriteLine("Сгенерированный массив:");
+            PrintArray(customArr.Array);
+
+
+            double sum = customArr.GetSumUntilTheLastPositiveElement();
+            Console.WriteLine("\nСумма элементов до последнего положительного элемента: " + sum);
+
+            Console.Write("\nВведите значение a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            customArr.CompressArray(a, b);
+
+            Console.WriteLine("\nМассив после сжатия:");
+
+            PrintArray(customArr.Array);
+
+            Console.WriteLine("\nЧасть 2:");
+
+            var secondPart = new SecondPart(4);
+            PrintMatrix(secondPart.Matrix);
+
+            var summ = secondPart.SumOfColumnsWithoutNegativeElements();
+            Console.WriteLine("Сумма элементов в тех столбцах, которые не содержат отрицательных эелементов: " + summ);
+
+            var min = secondPart.MinimumSumOfAbsDiagonals();
+            Console.WriteLine("Минимум среди сумм модулей элементов диагоналей, параллельных побочной диагонали матрицы: " + min);
+
+            Console.WriteLine("\nНажмите Enter, чтобы завершить программу.");
+            Console.ReadLine();
+
+        }
+
+        static void PrintArray(double[] array)
+        {
+            foreach (double element in array)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ M1/Task01_02_/Labs01_02/Labs01_02/SecondPart.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO_labs_task1
+{
+    public class SecondPart
+    {
+        private readonly int[,] matrix;
+
+        public SecondPart(int size)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+            matrix = new int[size, size];
+
+            var random = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(-10, 10);
+                }
+            }
+        }
+        public int SumOfColumnsWithoutNegativeElements()
+        {
+            int sum = 0;
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                bool hasNegative = false;
+                int columnSum = 0;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    if (matrix[row, col] < 0)
+                    {
+                        hasNegative = true;
+                        break;
+                    }
+                    columnSum += matrix[row, col];
+                }
+
+                if (!hasNegative)
+                {
+                    sum += columnSum;
+                }
+            }
+
+            return sum;
+        }
+
+        public int MinimumSumOfAbsDiagonals()
+        {
+            int minSum = int.MaxValue;
+            int size = matrix.GetLength(0);
+
+            for (int k = 1 - size; k < size; k++)
+            {
+                int currentSum = 0;
+                for (int i = 0; i < size; i++)
+                {
+                    int j = size - 1 - i - k;
+                    if (j >= 0 && j < size)
+                    {
+                        currentSum += Math.Abs(matrix[i, j]);
+                    }
+                }
+
+                if (currentSum < minSum)
+                {
+                    minSum = currentSum;
+                }
+            }
+
+            return minSum;
+        }
+
+        public int[,] Matrix
+        {
+            get { return matrix; }
+        }
+    }
+}
